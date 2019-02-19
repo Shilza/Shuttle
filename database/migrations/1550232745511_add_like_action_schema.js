e70f2eaedbd1c1e9a83f40558f57563907457dee
Shilza <andgalatinov@gmail.com>
2019-02-19 19:25:01 +0300

    Trigger fixes

--- database/migrations/1550232745511_add_like_action_schema.js
+++ database/migrations/1550232745511_add_like_action_schema.js
@@ -8,7 +8,7 @@ class AddLikeActionSchema extends Schema {
     async up() {
         await Database
             .raw(
-                'CREATE TRIGGER `add_like_action` AFTER INSERT ON `likes ' +
+                'CREATE TRIGGER `add_like_action` AFTER INSERT ON `likes` ' +
                 'FOR EACH ROW INSERT INTO actions (receiver_id, initiator_id, type, entity_id, created_at)' +
                 'VALUES(IF(NEW.type=1, (SELECT owner_id FROM posts WHERE id=NEW.entity_id), (SELECT owner_id FROM comments WHERE id=NEW.entity_id)), ' +
                 'NEW.owner_id, 1, NEW.id, NOW())'
