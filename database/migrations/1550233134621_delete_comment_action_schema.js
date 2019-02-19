821c324a0b0112f3617947c30eeb66b6b6741a3a
Shilza <andgalatinov@gmail.com>
2019-02-19 20:09:23 +0300

    Trigger fixes

--- database/migrations/1550233134621_delete_comment_action_schema.js
+++ database/migrations/1550233134621_delete_comment_action_schema.js
@@ -9,9 +9,7 @@ class DeleteCommentActionSchema extends Schema {
         await Database
             .raw(
                 'CREATE TRIGGER `delete_comment_action` BEFORE DELETE ON `comments`' +
-                'FOR EACH ROW DELETE FROM actions' +
-                'WHERE receiver_id = (SELECT owner_id FROM posts WHERE OLD.post_id)' +
-                'AND initiator_id=OLD.owner_id AND type=2 AND entity_id=OLD.id'
+                'FOR EACH ROW DELETE FROM actions WHERE receiver_id = (SELECT owner_id FROM posts WHERE OLD.post_id) AND initiator_id=OLD.owner_id AND type=2 AND entity_id=OLD.id'
             );
     }
 
