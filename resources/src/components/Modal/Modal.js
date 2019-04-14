8e6191020979df3d7e7f005d9d72ffad76f05f01
Shilza <andgalatinov@gmail.com>
2019-04-14 11:40:47 +0300

    Fix modal background scrolling

--- resources/src/components/Modal/Modal.js
+++ resources/src/components/Modal/Modal.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PropTypes from 'prop-types';
 import {createPortal} from "react-dom";
 import CloseButton from "./CloseButton";
 import styles from './modal.module.css';
 
 const Modal = ({children, closeModal}) => {
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => document.body.style.overflow = "visible";
+    }, []);
+
     const closeByCoverClick = event => {
         if(event.target.id === 'modalCover')
             closeModal();
