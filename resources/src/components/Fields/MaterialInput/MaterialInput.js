37726bdd86c92a898ea2a11f8336eaa0f1b8ebd1
Shilza <andgalatinov@gmail.com>
2019-04-14 11:42:05 +0300

    Material input ref forwarding

--- resources/src/components/Fields/MaterialInput/MaterialInput.js
+++ resources/src/components/Fields/MaterialInput/MaterialInput.js
@@ -2,20 +2,19 @@ import React from "react";
 import PropTypes from 'prop-types';
 import styles from './materialInput.module.css';
 
-const MaterialInput = ({label, onChange, defaultValue}) => (
+const MaterialInput = React.forwardRef(({label, onChange, defaultValue}) => (
     <div className={styles.group}>
         <input type="text" required defaultValue={defaultValue} onChange={onChange}/>
         <span className={styles.highlight}/>
         <span className={styles.bar}/>
         <label className={styles.label}>{label}</label>
     </div>
-);
+));
 
 MaterialInput.propTypes = {
     label: PropTypes.node,
     onChange: PropTypes.func,
     defaultValue: PropTypes.string
 };
-
 
 export default MaterialInput;
