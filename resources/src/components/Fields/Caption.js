225e8d490c87317a2e41303986f51a87a3cd9a68
Shilza <andgalatinov@gmail.com>
2019-04-14 11:55:45 +0300

    Caption Field

---
+++ resources/src/components/Fields/Caption.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Input } from "antd";
+import FormItem from "antd/es/form/FormItem";
+
+const { TextArea } = Input;
+
+const Caption = ({ getFieldDecorator, initialValue='' }) => {
+    return (
+        <FormItem>
+            {getFieldDecorator('caption', {
+                rules: [
+                    {max: 1000, message: 'Caption must be less than 1000 characters!'},
+                ], initialValue
+            })(
+                <TextArea rows={4} placeholder="Caption"/>
+            )}
+        </FormItem>
+    );
+};
+
+Caption.propTypes = {
+    getFieldDecorator: PropTypes.func.isRequired,
+    initialValue: PropTypes.string
+};
+
+export default Caption;
