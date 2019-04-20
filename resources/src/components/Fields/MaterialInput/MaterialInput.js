df8ed1c34592b73d17dd11dad34130e30130ecdb
Shilza <andgalatinov@gmail.com>
2019-04-20 19:04:02 +0300

    Fix forward ref error

--- resources/src/components/Fields/MaterialInput/MaterialInput.js
+++ resources/src/components/Fields/MaterialInput/MaterialInput.js
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from 'prop-types';
 import styles from './materialInput.module.css';
 
-const MaterialInput = React.forwardRef(({label, onChange, defaultValue}) => (
+const MaterialInput = ({label, onChange, defaultValue}) => (
     <div className={styles.group}>
         <input type="text" required defaultValue={defaultValue} onChange={onChange}/>
         <span className={styles.highlight}/>
         <span className={styles.bar}/>
         <label className={styles.label}>{label}</label>
     </div>
-));
+);
 
 MaterialInput.propTypes = {
     label: PropTypes.node,
