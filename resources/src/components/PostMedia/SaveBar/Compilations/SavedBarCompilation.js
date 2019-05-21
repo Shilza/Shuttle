1525e26ae2832ef423584fab70c89a7348d762bc
Shilza <andgalatinov@gmail.com>
2019-05-21 22:48:50 +0300

    SaveBarCompilation pass props fixes

--- resources/src/components/PostMedia/SaveBar/Compilations/SavedBarCompilation.js
+++ resources/src/components/PostMedia/SaveBar/Compilations/SavedBarCompilation.js
@@ -13,7 +13,8 @@ const SavedBarCompilation = ({compilation, dispatch, postId}) => {
     const setCompilationToSave = (event, compilationName) => {
         event.stopPropagation();
         setLoading(true);
-        dispatch(PostService.save({post_id: postId, compilation: compilationName}));
+        dispatch(PostService.save({post_id: postId, compilation: compilationName}))
+            .then(() => setLoading(false));
     };
 
     const icon = <Icon type="loading" style={{fontSize: 24}} spin/>;
@@ -21,7 +22,7 @@ const SavedBarCompilation = ({compilation, dispatch, postId}) => {
         <Spin spinning={loading} indicator={icon}>
             <div className={styles.compilation}
                  onClick={event => setCompilationToSave(event, Object.keys(compilation)[0])}>
-                <Compilation compilation={compilation} loadPosts={() => {}}/>
+                <Compilation compilation={compilation}/>
             </div>
         </Spin>
     );
@@ -30,11 +31,11 @@ const SavedBarCompilation = ({compilation, dispatch, postId}) => {
 SavedBarCompilation.propTypes = {
     compilation: PropTypes.object.isRequired,
     dispatch: PropTypes.func.isRequired,
-    postId: PropTypes.number.isRequired
+    postId: PropTypes.number
 };
 
 const mapStateToProps = state => ({
-    postId: state.saved.postToBeSaved.id
+    postId: state.saved.postToBeSaved && state.saved.postToBeSaved.id
 });
 
 export default connect(mapStateToProps)(SavedBarCompilation);
