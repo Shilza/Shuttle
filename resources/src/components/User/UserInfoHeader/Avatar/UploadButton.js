5894183b75a4375946981e0bcb5692ffeb97f52f
Shilza <andgalatinov@gmail.com>
2019-05-21 23:00:38 +0300

    Change upload avatar process

--- resources/src/components/User/UserInfoHeader/Avatar/UploadButton.js
+++ resources/src/components/User/UserInfoHeader/Avatar/UploadButton.js
@@ -1,43 +1,42 @@
 import React, {useRef, useState} from "react";
-import PropTypes from 'prop-types';
 import {Icon} from "antd";
 import styles from './avatar.module.css';
-import {connect} from "react-redux";
-import {message} from "antd/lib/index";
-import {updateAvatar} from "../../../../services/user";
+import Modal from "../../../Modal/Modal";
+import AvatarUploader from "./AvatarUploader";
 
 const inputStyle = {display: 'none'};
 
-const UploadButton = ({dispatch}) => {
+export const UploadButton = () => {
 
     let fileRef = useRef();
 
     let [media, setMedia] = useState(undefined);
 
     const loadMedia = event => {
-        setMedia(event.target.files[0]);
-        uploadAvatar();
-    };
+        const file = event.target.files[0];
+        let reader  = new FileReader();
 
-    const uploadAvatar = () => {
-        let avatar = new FormData();
-        avatar.append('avatar', media);
+        reader.onloadend = function () {
+            setMedia(reader.result);
+        };
 
-        dispatch(updateAvatar(avatar))
-            .then(data => message.success(data.message))
-            .catch(err => message.error(err.response.data.message));
+        if (file)
+            reader.readAsDataURL(file);
     };
 
     return (
-        <button className={styles.avatarActionButton} onClick={() => fileRef.current.click()}>
-            <Icon type='upload'/>
-            <input type='file' style={inputStyle} onChange={loadMedia} ref={fileRef}/>
-        </button>
+        <>
+            <button className={styles.avatarActionButton} onClick={() => fileRef.current.click()}>
+                <Icon type='upload'/>
+                <input type='file' style={inputStyle} onChange={loadMedia} ref={fileRef}/>
+            </button>
+            {
+                !!media &&
+                <Modal
+                    closeModal={() => {setMedia(undefined)}}>
+                    <AvatarUploader onClose={() => setMedia(undefined)} media={media}/>
+                </Modal>
+            }
+        </>
     )
 };
-
-UploadButton.propTypes = {
-    dispatch: PropTypes.func.isRequired
-};
-
-export default connect()(UploadButton);
