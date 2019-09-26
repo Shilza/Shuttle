a8d5e0931ef2b9bbdf6f698aff09c9a90b8cf23f
Shilza <evertonnaxoi@gmail.com>
2019-09-26 13:39:23 +0300

    Add Posts/Mark

---
+++ resources/src/components/Posts/Mark/Mark.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import PropTypes from 'prop-types';
+
+import styles from "./mark.module.css";
+
+const Mark = ({mark, link = false, text = 'Who is it?', ...props}) => (
+  <>
+    {
+      link ?
+        <Link
+          to={`/${mark.username}`}
+          className={styles.mark}
+          style={{
+            top: `${mark.top}%`,
+            left: `${mark.left}%`
+          }}
+          {...props}
+        >
+          {mark.username || text}
+        </Link>
+        :
+        <div
+          className={styles.mark}
+          style={{
+            top: `${mark.top}%`,
+            left: `${mark.left}%`
+          }}
+          {...props}
+        >
+          {mark.username || text}
+        </div>
+    }
+  </>
+);
+
+Mark.propTypes = {
+  mark: PropTypes.shape({
+    top: PropTypes.number.isRequired,
+    left: PropTypes.number.isRequired,
+    username: PropTypes.string
+  }),
+  test: PropTypes.string,
+  link: PropTypes.bool
+};
+
+export default Mark;
