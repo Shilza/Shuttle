d7a16a358924cca625aa4499dae8037aaabe7135
Shilza <evertonnaxoi@gmail.com>
2019-10-28 23:11:36 +0300

    Add react-svg

--- resources/src/components/SvgIcon/SvgIcon.js
+++ resources/src/components/SvgIcon/SvgIcon.js
@@ -1,14 +1,21 @@
 import React from "react";
+import ReactSVG from 'react-svg'
+import {Icon} from "antd";
+import Loader from "components/Paginator/Loader";
 import styles from "./svgIcon.module.css";
 
-const SvgIcon = React.memo(({icon, className, ...props}) => (
-  <div className={`${styles.icon} ${className}`}
-       style={{
-         mask: `url(${icon}) no-repeat 50% 50%`,
-         WebkitMask: `url(${icon}) no-repeat 50% 50%`
-       }}
-       {...props}
-  />
+const SvgIcon = React.memo(({icon, className, width = 20, height = 20, ...props}) => (
+  <div {...props}>
+    <ReactSVG
+      src={icon}
+      loading={() => <Loader/>}
+      fallback={() => <Icon type={'settings'}/>}
+      className={`${styles.icon} ${className}`}
+      beforeInjection={svg => {
+        svg.setAttribute('style', `width: ${width}px; height: ${height}px`)
+      }}
+    />
+  </div>
 ));
 
 export default SvgIcon;
