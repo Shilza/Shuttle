65099c05ea44f79147724a4dba946d567f8e3731
Shilza <evertonnaxoi@gmail.com>
2019-11-02 22:55:53 +0300

    Add db trigger on new mark

---
+++ database/migrations/1572722935239_add_mark_action_trigger_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+const Database = use('Database');
+
+class AddMarkActionTriggerSchema extends Schema {
+  async up() {
+    await Database
+      .raw(
+        'CREATE TRIGGER `add_mark_action` AFTER INSERT ON `marks`' +
+        'FOR EACH ROW BEGIN' +
+        'IF (SELECT owner_id FROM posts WHERE id = NEW.post_id) <> (SELECT id FROM users WHERE username = NEW.username)' +
+        'THEN' +
+        'INSERT INTO actions (receiver_id, initiator_id, type, entity_id, created_at)VALUES((SELECT id FROM users WHERE username=NEW.username), (SELECT owner_id FROM posts WHERE id = NEW.post_id), 4, NEW.post_id, NOW());' +
+        'END IF;' +
+        'END'
+      );
+  }
+
+  async down() {
+    await Database
+      .raw('DROP TRIGGER `add_mark_action`');
+  }
+}
+
+module.exports = AddMarkActionTriggerSchema;
