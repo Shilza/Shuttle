65fefe64123a0d742e150e187a64e6af505b6e2b
Shilza <evertonnaxoi@gmail.com>
2019-11-15 22:51:53 +0300

    Add stories for Button, IconButton, Modal components

--- frontend/src/ui/Button/Button.stories.js
+++ frontend/src/ui/Button/Button.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from "./Button";
-import 'styles/styles.css';
 
 export default { title: 'Button' };
 
@@ -8,6 +7,6 @@ export const simple = () => <Button>Simple button</Button>;
 
 export const disabled = () => <Button disabled>Disabled</Button>;
 
-export const loading = () => <Button loading>Text</Button>;
+export const loading = () => <Button loading>Loading</Button>;
 
 export const withTitle = () => <Button title='Sample'>Title on hover</Button>;
