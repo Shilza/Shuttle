be61b0c59884075d27eae8ed6d57a6c986a66a88
Shilza <evertonnaxoi@gmail.com>
2019-11-20 20:54:36 +0300

    Add delete method to MarkController

--- app/Controllers/Http/MarkController.js
+++ app/Controllers/Http/MarkController.js
@@ -1,6 +1,37 @@
 'use strict'
+const Mark = use('App/Models/Mark');
 
 class MarkController {
+  async delete({request, response, auth}) {
+
+    const rules = {
+      id: 'required|integer'
+    };
+
+    const validation = await validate(request.all(), rules);
+
+    if (validation.fails())
+      return response.status(400).json({
+        message: validation.messages()[0].message
+      });
+
+    const mark = await Mark.find(request.input('id'));
+
+    if (!mark)
+      return response.status(400).json({
+        message: 'Mark does not exists'
+      });
+
+    const user = await auth.getUser();
+    if (user.username !== mark.username)
+      return response.status(403).json({
+        message: 'Forbidden. Unable to delete'
+      });
+
+    await mark.delete();
+
+    response.json({message: 'Mark deleted successfully'});
+  }
 }
 
-module.exports = MarkController
+module.exports = MarkController;
