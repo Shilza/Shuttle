3a1b46bea69efe7a9deb1ebcbda7365720037bfd
Shilza <evertonnaxoi@gmail.com>
2020-03-14 11:22:18 +0300

    Fix migrations

--- database/migrations/1550232902545_add_comment_action_schema.js
+++ database/migrations/1550232902545_add_comment_action_schema.js
@@ -8,12 +8,12 @@ class AddCommentActionSchema extends Schema {
   async up() {
     await Database
       .raw(
-        'CREATE TRIGGER `add_comment_action` AFTER INSERT ON `comments`' +
+        'CREATE TRIGGER `add_comment_action` AFTER INSERT ON `comments` ' +
         'FOR EACH ROW BEGIN ' +
-        'IF (SELECT owner_id FROM posts WHERE id=new.post_id) <> NEW.owner_id' +
-        'THEN' +
-        'INSERT INTO actions (receiver_id, initiator_id, type, entity_id, created_at)VALUES((SELECT owner_id FROM posts WHERE id=new.post_id),NEW.owner_id, 2, NEW.id, NOW());' +
-        'END IF;' +
+        'IF (SELECT owner_id FROM posts WHERE id=new.post_id) <> NEW.owner_id ' +
+        'THEN ' +
+        'INSERT INTO actions (receiver_id, initiator_id, type, entity_id, created_at)VALUES((SELECT owner_id FROM posts WHERE id=new.post_id),NEW.owner_id, 2, NEW.id, NOW()); ' +
+        'END IF; ' +
         'END'
       );
   }
